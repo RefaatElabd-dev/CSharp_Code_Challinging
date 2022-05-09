08d19db150f89ead08dcd9acec38025cf2f4603c
RefaatElabd-dev <RefaatElabd7@gmail.com>
2022-05-09 11:07:13 +0200

    write code in the fly

---
+++ NullOperators/CodeHelper.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NullOperators
+{
+    internal class CodeHelper
+    {
+        public static string UseCNullCoalescingAssignment(string s = null)
+        {
+            //The ??= operator assigns a variable only if it’s nul
+            // if (s == null) s = "Hello, world";
+
+            return s ??= "string instead Of NULL.";
+        }
+    }
+}
---
+++ NullOperators/Program.cs
@@ -0,0 +1,7 @@
+﻿using System;
+using NullOperators;
+
+Console.WriteLine($"When s = Refaat ====> Output = {CodeHelper.UseCNullCoalescingAssignment("Refaat")}");
+Console.WriteLine($"\nWhen s = 'NULL' ====> Output = {CodeHelper.UseCNullCoalescingAssignment()}");
+
+Console.ReadLine();
