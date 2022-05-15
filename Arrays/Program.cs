5054dd4420496cfdc6d6811be158c2ba9b05f5bd
RefaatElabd-dev <RefaatElabd7@gmail.com>
2022-05-15 13:10:42 +0200

    work with arrays

---
+++ Arrays/Program.cs
@@ -0,0 +1,101 @@
+﻿using System;
+
+namespace Arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Rectangular arrays
+            int[,] matrix = new int[3, 3];
+
+            Console.WriteLine(matrix.Length);
+
+            Console.WriteLine(matrix.GetLength(0));
+            Console.WriteLine(matrix.GetLength(1));
+            //Console.WriteLine(matrix.GetLength(2));  out of index exception
+
+            Console.ReadLine();
+            for (int i = 0; i < matrix.GetLength(0); i++)
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                    matrix[i, j] = i * 3 + j + 1;
+
+            for (int i = 0; i < matrix.GetLength(0); i++)
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                    Console.WriteLine(matrix[i,j]);
+
+            var rectMatrix = new int[,] // rectMatrix is implicitly of type int[,]
+                            {
+                                {0,1,2},
+                                {3,4,5},
+                                {6,7,8}
+                            };
+
+            for (int i = 0; i < rectMatrix.GetLength(0); i++)
+                for (int j = 0; j < rectMatrix.GetLength(1); j++)
+                    Console.WriteLine(rectMatrix[i, j]);
+            
+            Console.ReadLine();
+            #endregion
+
+            #region Jagged arrays         each element is an array
+            int[][] matrix2 = new int[3][]
+            {
+                new int[] {1},
+                new int[] {2, 3},
+                new int[] {4, 5, 6}
+            };
+
+            for (int i = 0; i < matrix2.GetLength(0); i++)
+                for (int j = 0; j < matrix2[i].GetLength(0); j++)
+                    Console.WriteLine(matrix2[i][j]);
+            Console.ReadLine();
+            #endregion
+
+            #region params parameters
+
+            int total = Sum(1, 2, 3, 4);
+            int total2 = Sum(new int[] { 1, 2, 3, 4 });
+
+            int Sum(params int[] ints)
+            {
+                int sum = 0;
+                for (int i = 0; i < ints.Length; i++)
+                    sum += ints[i]; // Increase sum by ints[i]
+                return sum;
+            }
+            Console.WriteLine(total);
+            Console.WriteLine(total2);
+
+            Console.ReadLine();
+
+
+            double dTotal = Sum2(15, 1, 2, 3, 4);
+            double dTotal2 = Sum2(15.0, new int[] { 1, 2, 3, 4 });
+
+            double Sum2(double y, params int[] ints)
+            {
+                int sum = 0;
+                for (int i = 0; i < ints.Length; i++)
+                    sum += ints[i]; // Increase sum by ints[i]
+                return sum + y;
+            }
+
+            Console.WriteLine(dTotal);
+            Console.WriteLine(dTotal2);
+            Console.ReadLine();
+
+            #endregion
+
+            #region Ref Locals
+            int[] numbers = { 0, 1, 2, 3, 4 };
+            ref int numRef = ref numbers[2];
+
+            numRef *= 10;
+            Console.WriteLine(numRef); // 20
+            Console.WriteLine(numbers[2]); // 20
+            Console.ReadLine();
+            #endregion
+        }
+    }
+}
---
+++ Conversions/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+
+namespace Conversions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 100000007;
+            Console.WriteLine(i);
+            float f = i;
+            Console.WriteLine(f);
+            int i2 = (int)f;
+            Console.WriteLine(i2);
+            Console.ReadKey();
+
+            int l = 100000004;
+            Console.WriteLine(l);
+            float f2 = l;
+            Console.WriteLine(f2);
+            long l2 = (long)f2;
+            Console.WriteLine(l2);
+            Console.ReadKey();
+
+            //Checked operator
+
+            int a = 1000000, b = 1000000;
+            Console.WriteLine((a * b));
+
+            Console.WriteLine("############  Special floating point values  ##########");
+            Console.WriteLine(double.NaN);
+            Console.WriteLine(1.0 / 0.0); // Infinity
+            Console.WriteLine(-1.0 / 0.0); // -Infinity
+            Console.WriteLine(1.0 / -0.0); // -Infinity
+            Console.WriteLine(-1.0 / -0.0); // Infinity
+            Console.WriteLine(0.0 / 0.0); // NaN
+            Console.WriteLine((1.0 / 0.0) - (1.0 / 0.0)); // NaN
+
+            Console.WriteLine(0.0 / 0.0 == double.NaN); // False
+            Console.ReadKey();
+
+
+            Console.WriteLine("############  Casting issues with small memory predefined Types ##########");
+
+            short x = 3;
+            short y = 7;
+
+            //short z = x + y;  issue as + op by default return int
+            short z = (short) (x + y);
+            Console.WriteLine(z);
+            Console.ReadKey();
+
+
+            Console.WriteLine("############  Shift Operators  ##########");
+
+            Console.WriteLine("############  Lift  ##########");
+            int lOP = 0b0101_0011_0000_0100_1101_0011_0000_0000;
+            int aLOP = lOP << 2;
+            Console.WriteLine(lOP);
+            Console.WriteLine(Convert.ToString(lOP, toBase: 2));
+            Console.WriteLine(aLOP);
+            Console.WriteLine(Convert.ToString(aLOP, toBase: 2));
+
+            Console.WriteLine("############  Right  ##########");
+            int rOP = 0b1101_0011_0000_0100;
+            int aROP = rOP >> 2;
+            Console.WriteLine(rOP);
+            Console.WriteLine(Convert.ToString(rOP, toBase: 2));
+            Console.WriteLine(aROP);
+            Console.WriteLine(Convert.ToString(aROP, toBase: 2));
+
+            Console.ReadKey();
+        }
+
+    }
+}
