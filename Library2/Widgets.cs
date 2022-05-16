83d65258ad2863ee4ae07e504bf9e4c928b0d8dc
RefaatElabd-dev <RefaatElabd7@gmail.com>
2022-05-16 14:20:31 +0200

    dealing with NameSpaces

---
+++ Library2/Widgets.cs
@@ -0,0 +1,6 @@
+﻿using System;
+
+namespace Widgets
+{
+    public class Widget { }
+}
---
+++ MainApp/Program.cs
@@ -0,0 +1,4 @@
+﻿extern alias W1;
+extern alias W2;
+W1.Widgets.Widget w1 = new W1.Widgets.Widget();
+W2.Widgets.Widget w2 = new W2.Widgets.Widget();
